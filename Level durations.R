5936a8ce10a17929d7b2a43aeb0a1709d905f25a
gibbz00 <showvik.sizan@gmail.com>
2016-06-08 15:14:42 -0400

    first time commit

---
+++ Level durations.R
@@ -0,0 +1,13 @@
+df2$DurationLastLevel<- as.numeric(df2$DurationLastLevel)
+df2[, TotalDaysUntilRecent := cumsum(DurationLastLevel), by = ContactID]
+
+write.csv(df2, "C:/Users/ssizan/Documents/SQL_CSV/FullLifeCycle.csv",row.names = FALSE)
+
+df2[ , BecomesProducer:=ifelse(any(EngagementLevel1=="New Producer") , "TRUE", "FALSE")
+ , by = ContactID]
+
+df2[ , FollowsCLC:=ifelse(any(grepl("Level0-->Level1-->Level2-->Level3-->Level4-->Level5",FullLevelCycle)) , "TRUE", "FALSE")
+     , by = ContactID]
+
+
+grepl("Level0-->Level1-->Level2-->Level3-->Level4-->Level5",df2$FullLevelCycle)
